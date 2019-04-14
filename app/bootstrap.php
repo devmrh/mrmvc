c164c3503e4017728c4f3379b7a745d66689ac62
devmrh <dev.mohammadreza@yahoo.com>
2019-04-14 17:23:18 +0430

    add config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
-  //todo: require in all library classes
 
+require_once('config/config.php');
 
-require_once 'lib/Core.php';
-require_once 'lib/Controller.php';
-// spl_autoload_register(function ($className) {
-//   require_once 'lib/' . $className . '.php';
-// });
+spl_autoload_register(function ($className) {
+  require_once 'lib/' . $className . '.php';
+});
---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php 
+  //here we impalement our configs
+
+  // our data base constants 
+
+  define('DB_HOST','localhost');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', 'root');
+  define('DB_NAME', 'mrmvc');
+
+  //define app root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // define url root
+  define('URLROOT', 'http://mrmvc.com');
+
+  //define site name
+  define('SITENAME','mrmvc');
